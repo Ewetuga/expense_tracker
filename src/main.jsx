c61fcb1523582458f8076abf6c2a2d164a2ecdd8
Ewetuga <109183013+Ewetuga@users.noreply.github.com>
2025-06-15 13:17:00 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './App.css'; // or './App.css' depending on your setup
+
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
